752deeb22d1bafc2906430f2afa31fb23d5812a3
kouichiUme <kouichiume@gmail.com>
2017-03-28 02:01:13 +0900

    stub method and unit test

--- src/code4.nagoya/order/order.go
+++ src/code4.nagoya/order/order.go
@@ -1,7 +1,11 @@
 package order
 
 import (
+	"errors"
 	"fmt"
+	//	"strconv"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
 type OrderChainCode struct {
@@ -9,6 +13,58 @@ type OrderChainCode struct {
 
 func (c *OrderChainCode) validate() {
 
-	fmt.Printf("test\n")
+	fmt.Printf("validate\n")
 
+}
+
+func (c *OrderChainCode) init() {
+
+	fmt.Printf("init\n")
+}
+
+func (c *OrderChainCode) invoke() ([]byte, error) {
+
+	fmt.Printf("invoke\n")
+	return nil, nil
+}
+
+func (c *OrderChainCode) delete() ([]byte, error) {
+
+	fmt.Printf("delete\n")
+	return nil, nil
+}
+
+func (c *OrderChainCode) query() ([]byte, error) {
+
+	fmt.Printf("query\n")
+
+	return nil, nil
+
+}
+
+func (c *OrderChainCode) Init(stub shim.ChaincodeStubInterface) ([]byte, error) {
+
+	_, args := stub.GetFunctionAndParameters()
+
+	fmt.Println(args)
+
+	return nil, nil
+
+}
+
+//
+func (c *OrderChainCode) Invoke(stub shim.ChaincodeStubInterface) ([]byte, error) {
+	function, _ := stub.GetFunctionAndParameters()
+	if function == "invoke" {
+		// Make payment of X units from A to B
+		return c.invoke()
+	} else if function == "delete" {
+		// Deletes an entity from its state
+		return c.delete()
+	} else if function == "query" {
+		// the old "Query" is now implemtned in invoke
+		return c.query()
+	}
+
+	return nil, errors.New("Invalid invoke function name. Expecting \"invoke\" \"delete\" \"query\"")
 }
--- src/code4.nagoya/order/order_test.go
+++ src/code4.nagoya/order/order_test.go
@@ -2,6 +2,7 @@ package order
 
 import (
 	"fmt"
+	//	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"testing"
 )
 
@@ -11,7 +12,57 @@ func TestOrderChainCodeValidate(t *testing.T) {
 
 	orderChainCode := new(OrderChainCode)
 	orderChainCode.validate()
-	
+
 	fmt.Println("end order chaincode validate")
 
 }
+
+func TestOrderChainCodeInit(t *testing.T) {
+
+	fmt.Printf("start  order chaincode init\n")
+
+	orderChainCode := new(OrderChainCode)
+	//	orderChainCode.Init(new(shim.ChaincodeStubInterface))
+	orderChainCode.init()
+	fmt.Println("end order chaincode init")
+
+}
+
+func TestOrderChainCodeInvoke(t * testing.T){
+	fmt.Println("start order chaincode invoke" )
+	
+	orderChainCode := new(OrderChainCode)
+	orderChainCode.invoke()
+	fmt.Println("end order chaincode invoke " )
+}
+
+
+func TestOrderChainCodeDelete(t *testing.T){
+	
+	fmt.Println("start ChainCode Delete" )
+	
+
+	orderChainCode := new(OrderChainCode)
+	//	orderChainCode.Init(new(shim.ChaincodeStubInterface))
+	orderChainCode.delete()
+	
+	
+	
+	fmt.Println("end ChainCode Delete")
+	
+}
+
+//
+//
+// test query
+func TestOrderChainCodeQuery(t *testing.T) {
+
+	fmt.Printf("start  order chaincode init\n")
+
+	orderChainCode := new(OrderChainCode)
+	//	orderChainCode.Init(new(shim.ChaincodeStubInterface))
+	orderChainCode.query()
+	fmt.Println("end order chaincode init")
+
+}
+
